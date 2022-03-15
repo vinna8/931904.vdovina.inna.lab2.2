7654b3ae45e812349e8f6fa0a3bbcb9072b3ecc5
vinna8 <90774751+vinna8@users.noreply.github.com>
2022-03-15 22:57:18 +0700

    lab 2

---
+++ WebApp2/WebApp2/Controllers/CalcController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp2.Models;
+
+namespace WebApp2.Controllers
+{
+    public class CalcController : Controller
+    {
+        private readonly ILogger<CalcController> _logger;
+
+        public CalcController(ILogger<CalcController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Manual()
+        {
+            if (Request.Method == "POST")
+            {
+                try
+                {
+                    var calcModel = new CalcModel
+                    {
+                        x = Int32.Parse(HttpContext.Request.Form["x"]),
+                        Operation = HttpContext.Request.Form["operation"],
+                        y = Int32.Parse(HttpContext.Request.Form["y"])
+                    };
+
+                    ViewBag.Result = calcModel.Calc();
+                }
+                catch
+                {
+                    ViewBag.Result = "Invalid input";
+                }
+
+                return View("Result");
+            }
+            return View("ViewWithHtmlHelpers");
+        }
+
+        [HttpGet]
+        [ActionName("ManualWithSeparateHandlers")]
+        public IActionResult ManualWithSeparateHandlersGet()
+        {
+            return View("ViewWithHtmlHelpers");
+        }
+
+        [HttpPost]
+        [ActionName("ManualWithSeparateHandlers")]
+        public IActionResult ManualWithSeparateHandlersPost()
+        {
+            try
+            {
+                var calcModel = new CalcModel
+                {
+                    x = Int32.Parse(HttpContext.Request.Form["x"]),
+                    Operation = HttpContext.Request.Form["operation"],
+                    y = Int32.Parse(HttpContext.Request.Form["y"])
+                };
+
+                ViewBag.Result = calcModel.Calc();
+            }
+            catch
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+        [HttpGet]
+        public IActionResult ModelBindingInParameters()
+        {
+            return View("ViewWithHtmlHelpers");
+        }
+
+        [HttpPost]
+        public IActionResult ModelBindingInParameters(int x, string operation, int y)
+        {
+            if (ModelState.IsValid)
+            {
+                var calcModel = new CalcModel
+                {
+                    x = x,
+                    Operation = operation,
+                    y = y
+                };
+                ViewBag.Result = calcModel.Calc();
+            }
+            else
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+        [HttpGet]
+        public IActionResult ModelBindingInSeparateModel()
+        {
+            return View("ViewWithTagHelpers");
+        }
+
+        [HttpPost]
+        public IActionResult ModelBindingInSeparateModel(CalcModel model)
+        {
+            ViewBag.Result = ModelState.IsValid
+                ? model.Calc()
+                : "Invalid input";
+
+            return View("Result");
+        }
+    }
+}
+
---
+++ WebApp2/WebApp2/Models/CalcModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2.Models
+{
+    public class CalcModel
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public string Operation { get; set; }
+
+        public string Calc()
+        {
+            return Operation switch
+            {
+                "+" => $"{x} + {y} = {x + y}",
+                "-" => $"{x} - {y} = {x - y}",
+                "*" => $"{x} * {y} = {x * y}",
+                "/" => y != 0 ? $"{x} /  {y} = {x / y}" : "Division by zero",
+                _ => "Invalid operation"
+            };
+        }
+    }
+}
